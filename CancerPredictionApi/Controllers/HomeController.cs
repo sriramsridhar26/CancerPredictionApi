2c10b4130f3c604e6ea0b84f854b446711b5e3d1
sriramsridhar26 <sriramsridhar26@gmail.com>
2023-01-05 11:37:52 +0530

    created .net to python connection method

--- CancerPredictionApi/Controllers/HomeController.cs
+++ CancerPredictionApi/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using CancerPredictionApi.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
+using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using System;
+using System.IO;
+using System.Text;
 
 namespace CancerPredictionApi.Controllers
 {
@@ -16,20 +21,20 @@ namespace CancerPredictionApi.Controllers
         }
         [HttpPost("/UploadVid")]
         public async Task<IActionResult> UploadVid(IFormFile file)
-        { 
-                var filePath = "D:/down/TEMP.mp4";
-                using (var stream = System.IO.File.Create(filePath))
-                {
-                    await file.CopyToAsync(stream);
-                }
+        {
+            var filePath = "D:/down/TEMP.mp4";
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
             //Console.WriteLine("starttime:", temp.starttime);
             //Console.WriteLine("endtime:",temp.endtime);
             ServiceResponse<string> response = new ServiceResponse<string>();
-            response.Success= true;
+            response.Success = true;
             response.Message = "success";
             response.Data = "Temp";
-                return Ok(response);
-       
+            return Ok(response);
+
         }
         [HttpPost("/Strip")]
         public async Task<IActionResult> Strip(strip st)
@@ -41,7 +46,7 @@ namespace CancerPredictionApi.Controllers
         [HttpGet("/download")]
         public async Task<IActionResult> download()
         {
-            var filePath= "D:/down/TEMP.mp4";
+            var filePath = "D:/down/TEMP.mp4";
             //var provider = new FileExtensionContentTypeProvider();
             //if (!provider.TryGetContentType(filePath, out var contentType))
             //{
@@ -51,5 +56,37 @@ namespace CancerPredictionApi.Controllers
             var bytes = await System.IO.File.ReadAllBytesAsync(filePath);
             return File(bytes, "video/mp4", Path.GetFileName(filePath));
         }
+        [HttpGet("/dummy")]
+        public async Task<IActionResult> dummy()
+        {
+            string str = detect_cancer(12, 15, 25, 55, @"C:\Users\Gideon\Downloads\TEMP.mp4");
+            return Ok(str);
+            //return Ok();
+        }
+        public string detect_cancer(int rsme, int iniw, int finalw, int gain, string loc)
+        {
+            string lastline;
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = @"D:\conda\python.exe";
+            //start.Arguments = @"D:\App\App.py " + loc + " " + rsme + " " + iniw + " " + finalw + " " + gain;
+            start.Arguments = @"D:\App\App.py";
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    Console.WriteLine(result);
+                    string path = @"D:\" + DateTime.Now.ToString("ddMMyyyyhhmmss") + ".txt";
+                    using (StreamWriter sw = System.IO.File.CreateText(path)) ;
+                    System.IO.File.WriteAllText(path, result);
+                    lastline = System.IO.File.ReadLines(path).Last();
+                    Console.WriteLine(lastline);
+                }
+            }
+            return lastline;
+        }
+
     }
 }
