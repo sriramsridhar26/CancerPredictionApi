274f4ad063d41402dc3a0e268adfdba0c3e660c8
CAPED_CRUSADER01 <sriramsridhar26@gmail.com>
2023-01-07 22:24:05 +0530

    added predict endpoint

---
+++ CancerPredictionApi/Model/Param.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CancerPredictionApi.Model
+{
+    public class Param
+    {
+        [Required]
+        public string fileName { get; set; }
+        [Required]
+        public int rsme { get; set; }
+        [Required]
+        public int iniw { get; set; }
+        [Required]
+        public int finalw { get; set; }
+        [Required]
+        public int gain { get; set; }
+
+    }
+}
